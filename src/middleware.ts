6c479898b9d84b351d9cc410673b01d523197408
chloe gregoire <chle.gregire@gmail.com>
2026-04-05 11:25:14 +0200

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
-
 import { defineMiddleware } from "astro:middleware";
 
-const MAINTENANCE = import.meta.env.MAINTENANCE === "true" ;
-
 export const onRequest = defineMiddleware((context, next) => {
- if (MAINTENANCE && context.url.pathname !== "/maintenance") {
-  return new Response(
-    '<meta http-equiv="refresh" content="0; url=/maintenance" />',
-    { status: 302, headers: { "Content-Type": "text/html" } }
-  );
-}
+  const MAINTENANCE = import.meta.env.MAINTENANCE === "true"; 
+
+  if (MAINTENANCE && context.url.pathname !== "/maintenance") {
+    return Response.redirect(new URL("/maintenance", context.url), 302);
+  }
 
   return next();
 });
