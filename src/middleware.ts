a7f77ef3b028b9547ace8e458ddc83f07e8c838b
chloe gregoire <chle.gregire@gmail.com>
2026-04-05 11:37:26 +0200

    log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const MAINTENANCE = import.meta.env.MAINTENANCE === "true"; 
+  const MAINTENANCE = import.meta.env.MAINTENANCE === "true";
+  console.log("MAINTENANCE env:", import.meta.env.MAINTENANCE, typeof import.meta.env.MAINTENANCE);
 
   if (MAINTENANCE && context.url.pathname !== "/maintenance") {
     return Response.redirect(new URL("/maintenance", context.url), 302);
