575586975989596d590f14a194ffc6879338a898
chloe gregoire <chle.gregire@gmail.com>
2026-04-05 11:06:32 +0200

    redirection midleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,12 @@ import { defineMiddleware } from "astro:middleware";
 const MAINTENANCE = import.meta.env.MAINTENANCE === "true" ;
 
 export const onRequest = defineMiddleware((context, next) => {
-  if ( MAINTENANCE && context.url.pathname !== "/maintenance") {
-    return context.rewrite("/maintenance");
-  }
+ if (MAINTENANCE && context.url.pathname !== "/maintenance") {
+  return new Response(
+    '<meta http-equiv="refresh" content="0; url=/maintenance" />',
+    { status: 302, headers: { "Content-Type": "text/html" } }
+  );
+}
 
   return next();
 });
